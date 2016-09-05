bafa87a2788b6ffcf23b6bd88d78e73db414f927
alexey <AlexeyElijah@gmail.com>
2016-09-05 17:02:20 +0800

    三,第三步,实现activity类中Onclick编写

--- injectdemo/src/main/java/inject/zwc/com/injectdemo/MainActivity.java
+++ injectdemo/src/main/java/inject/zwc/com/injectdemo/MainActivity.java
@@ -11,10 +11,10 @@ import com.zwc.inject.annotation.BindViews;
 import com.zwc.inject.annotation.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.tv_container)
-    TextView mTvContainer;
-    @BindViews({R.id.tv_binds_container1,R.id.tv_binds_container2,R.id.tv_binds_container3,R.id.tv_binds_container4})
-    TextView[] mTvConArray;
+        @BindView(R.id.tv_container)
+        TextView mTvContainer;
+        @BindViews({R.id.tv_binds_container1, R.id.tv_binds_container2, R.id.tv_binds_container3, R.id.tv_binds_container4})
+        TextView[] mTvConArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +26,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void startBinds() {
         mTvContainer.setText("bind succsess");
-        for(int i=0;i<mTvConArray.length;i++){
-            mTvConArray[i].setText("bins+"+i+"   ");
+        for (int i = 0; i < mTvConArray.length; i++) {
+            mTvConArray[i].setText("bins+" + i + "   ");
         }
     }
+
     @OnClick(R.id.bt_click1)
-    public void onBt1Click(){
-        Toast.makeText(this,"按钮1被点击了",Toast.LENGTH_SHORT).show();
+    public void onBt1Click() {
+        Toast.makeText(this, "按钮1被点击了", Toast.LENGTH_SHORT).show();
     }
+
     @OnClick(R.id.bt_click2)
-    public void onBt2Click(){
-        Toast.makeText(this,"按钮2被点击了",Toast.LENGTH_SHORT).show();
+    public void onBt2Click() {
+        Toast.makeText(this, "按钮2被点击了", Toast.LENGTH_SHORT).show();
     }
+
     @OnClick(R.id.bt_click3)
-    public void onBt3Click(){
-        Toast.makeText(this,"按钮3被点击了",Toast.LENGTH_SHORT).show();
+    public void onBt3Click() {
+        Toast.makeText(this, "按钮3被点击了", Toast.LENGTH_SHORT).show();
     }
 }
